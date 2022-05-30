b1a8d6fff41a8b9b12e2ca4dbbdc951476bcaff2
speedcubing <speedcubingthebest@gmail.com>
2022-05-30 14:14:42 +0800

    obj

--- src/speedcubing/server/libs/User.java
+++ src/speedcubing/server/libs/User.java
@@ -1,13 +1,9 @@
 package speedcubing.server.libs;
 
 import org.apache.commons.lang.ArrayUtils;
-import speedcubing.lib.utils.Reflections;
 import speedcubing.server.speedcubingServer;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class User {
     public static Map<UUID, User> users = new HashMap<>();
@@ -18,9 +14,6 @@ public class User {
     public String rank;
     public int tcpPort;
 
-    public User() {
-    }
-
     public User(UUID uuid, String rank, Set<String> permissions) {
         this.lang = speedcubingServer.connection.selectInt("playersdata", "lang", "uuid='" + uuid + "'");
         this.rank = rank;
@@ -28,10 +21,6 @@ public class User {
         this.velocities = ArrayUtils.toPrimitive(speedcubingServer.veloStorage.get(uuid));
         this.tcpPort = speedcubingServer.tcpStorage.get(uuid);
         users.put(uuid, this);
-    }
-
-    public void init(Object o, UUID uuid) {
-        Reflections.copyParentFields(o, getUser(uuid));
     }
 
     public static User getUser(UUID uuid) {
