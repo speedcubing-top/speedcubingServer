c7b34eee0ce41504bc4ab1c96823efe7af2b0a39
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 16:32:14 +0800

    update

--- src/speedcubing/server/libs/LogListener.java
+++ src/speedcubing/server/libs/LogListener.java
@@ -30,7 +30,7 @@ public class LogListener {
                     }
                 }
                 if (!denied)
-                    speedcubingServer.tcp.sendClean(20000, "log|" + Bukkit.getServerName() + "|" + event.getLevel() + "|" + event.getNanoTime() + "|" + msg);
+                    speedcubingServer.tcp.sendClean(20000, "log|" + Bukkit.getServerName() + "|" + event.getLevel() + "|" + event.getTimeMillis() + "|" + msg);
                 return denied ? Result.DENY : null;
             }
 
