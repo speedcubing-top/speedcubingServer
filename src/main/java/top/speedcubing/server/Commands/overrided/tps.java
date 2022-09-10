0f21264c09cfa0d074f21a48c41f096e0d24457b
speedcubing <speedcubingthebest@gmail.com>
2022-09-10 13:54:20 +0800

    update

--- src/main/java/top/speedcubing/server/Commands/overrided/tps.java
+++ src/main/java/top/speedcubing/server/Commands/overrided/tps.java
@@ -5,14 +5,15 @@ import net.minecraft.server.v1_8_R3.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import top.speedcubing.lib.utils.TimeUtils;
 import top.speedcubing.server.commandoverrider.OverrideCommandManager;
 
 import java.lang.management.ManagementFactory;
 import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
 
 public class tps implements OverrideCommandManager.OverridedCommand {
     public void execute(CommandSender commandSender, String message) {
-
         int ticks = MinecraftServer.currentTick - MinecraftServer.firstTick;
         double[] tps = MinecraftServer.getServer().recentTps;
         double recenttps = (double) Math.round((1.0D - tps[0] / 20.0D) * 100.0D);
@@ -35,15 +36,11 @@ public class tps implements OverrideCommandManager.OverridedCommand {
                 + ", " + (total - free) / 1048576 + " (MB)§e (" + Math.round((total - free) * 10000D / runtime.maxMemory()) / 100 + "%)"
                 , "§6Total CPU Usage: §a" + new DecimalFormat("#.##").format(ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class).getSystemCpuLoad() * 100) + "%"
                 , "§6Online: §a" + Bukkit.getOnlinePlayers().size() + "/" + Bukkit.getMaxPlayers()
-                , "§6Uptime: §a" + format(ticks) + "§e (" + ticks + " ticks)"
+                , "§6Uptime: §a" + TimeUtils.unixToTime(ticks * 50L, "%d%d %h%h %m%m %s%s %ms%ms", TimeUnit.MILLISECONDS) + "§e (" + ticks + " ticks)"
                 , "§6Entities: §a" + entities
                 , "§6Loaded Chunks: §a" + chunks
                 , "§7-----------------------------"};
         commandSender.sendMessage(result);
-    }
-
-    public static String format(long seconds) {
-        return seconds / 1728000 + "d " + String.format("%02d", (seconds / 72000) % 24) + "h " + String.format("%02d", (seconds / 1200) % 60) + "m " + String.format("%02d", (seconds / 20) % 60) + "s";
     }
 
     private String format(double tps) {
