841c9ed990b6310015df9a3c1833070ca0a4b1f6
speedcubing <speedcubingthebest@gmail.com>
2022-09-24 01:09:35 +0800

    update

--- src/main/java/top/speedcubing/server/speedcubingServer.java
+++ src/main/java/top/speedcubing/server/speedcubingServer.java
@@ -243,7 +243,7 @@ public class speedcubingServer extends JavaPlugin {
         calcTimer.cancel();
         systemConnection.update(
                 "servers",
-                "onlinecount=0,ram_heap=0,ram_used=0,tps1=0,tps2=0,tps3=0",
+                "onlinecount=0,ram_max=0,ram_heap=0,ram_used=0,tps1=0,tps2=0,tps3=0",
                 "name='" + Bukkit.getServerName() + "'"
         );
     }
