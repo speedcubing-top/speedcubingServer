161be5238f7c6a35c9df3df8be1c2418c4cd50d9
speedcubing <speedcubingthebest@gmail.com>
2022-10-29 17:02:17 +0800

    update path

--- src/main/java/top/speedcubing/server/Commands/hub.java
+++ src/main/java/top/speedcubing/server/Commands/hub.java
@@ -4,7 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import top.speedcubing.lib.bukkit.BungeePluginMessage;
+import top.speedcubing.lib.bukkit.pluginMessage.BungeePluginMessage;
 import top.speedcubing.server.events.player.HubEvent;
 
 public class hub implements CommandExecutor {
