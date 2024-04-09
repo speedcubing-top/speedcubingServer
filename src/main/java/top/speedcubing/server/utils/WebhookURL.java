9de8c224c65eefa1bc3c8dc0422fd563dbf01af1
SeanMud0319 <whe91000@gmail.com>
2024-04-09 20:09:00 +0800

    Fixing enum name

--- src/main/java/top/speedcubing/server/utils/WebhookURL.java
+++ src/main/java/top/speedcubing/server/utils/WebhookURL.java
@@ -2,7 +2,7 @@ package top.speedcubing.server.utils;
 
 public enum WebhookURL {
     LOBBY("https://discord.com/api/webhooks/1226829359273345065/8NAdavQ9_c7c3VHuytzh3kYTAjd-zafutHXnaCvCyguzEpH_FZl3RYTv5Qf7LOus0gR8"),
-    KBFFA("https://discord.com/api/webhooks/1226846446314065920/rhNCvAgii9vlezhenaRebxD3my3jm4pGxKeBa2eh3nE-fpAPg7FcZp7Qi23f7rKvU36u"),
+    KNOCKBACKFFA("https://discord.com/api/webhooks/1226846446314065920/rhNCvAgii9vlezhenaRebxD3my3jm4pGxKeBa2eh3nE-fpAPg7FcZp7Qi23f7rKvU36u"),
     CLUTCH("https://discord.com/api/webhooks/1226846735691677798/NKx-EivJ0Hxb8NR1ReM-BMTjOhxV6XHySFNZGA3wch_ksd5iSzbQ-C-_hAuG0ky-5qxS"),
     MLGRUSH("https://discord.com/api/webhooks/1226846967590555648/5nMyrYY7eVpjPzXRrQ7ByYRClaMfwpzaTXtin5eBJRBPwzW_ipP9EpJmQtT2GaxhqlVY"),
     FASTBUILDER("https://discord.com/api/webhooks/1226847230510497922/_Ff7ylV7_bpAHA2aZuOOGS7aedkCWjIyCIRnj5eLtnlbqL-R_zAmhXnk2773GgWQTDu-"),
